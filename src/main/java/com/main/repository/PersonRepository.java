4af93186ed8039bf241f3ea2a91984f8c31f70c9
João Paulo Dias <jpdias1997@hotmail.com>
2017-09-05 01:18:58 -0300

    Commit ActivityArea

--- src/main/java/com/main/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.main.repository;
-
-import com.main.models.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-}
